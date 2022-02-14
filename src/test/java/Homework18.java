f95b3b97209c56308118c4edb5f58d9547ffa350
Ann <annarmash008@gmail.com>
2022-02-14 09:31:23 +0300

    Homework done

--- src/test/java/Homework18.java
+++ src/test/java/Homework18.java
@@ -1,19 +1,11 @@
 import com.codeborne.selenide.Configuration;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
-
-import java.util.concurrent.TimeUnit;
 
 import static com.codeborne.selenide.Selenide.open;
 import static org.testng.Assert.assertEquals;
 
 public class Homework18 {
-    private WebDriver driver;
-    private WebDriverWait wait;
 
     @BeforeTest
     public void before() {
