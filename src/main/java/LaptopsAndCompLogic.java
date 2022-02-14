3d813cc4a821e22e2c252fdd6c0a0426f1cdb958
Ann <annarmash008@gmail.com>
2022-02-14 09:33:02 +0300

    Homework done

--- src/main/java/LaptopsAndCompLogic.java
+++ src/main/java/LaptopsAndCompLogic.java
@@ -1,8 +1,6 @@
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.SelenideElement;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 import static com.codeborne.selenide.Selenide.*;
 
